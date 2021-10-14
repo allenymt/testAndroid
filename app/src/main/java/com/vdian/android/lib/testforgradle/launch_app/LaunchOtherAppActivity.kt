0426edabb705f1b08b1a808f0f4564cf372dea67
yulun <yulun@weidian.com>
2021-10-14 17:47:59 +0800

    抖音外投

--- app/src/main/java/com/vdian/android/lib/testforgradle/launch_app/LaunchOtherAppActivity.kt
+++ app/src/main/java/com/vdian/android/lib/testforgradle/launch_app/LaunchOtherAppActivity.kt
@@ -49,7 +49,11 @@ class LaunchOtherAppActivity : AppCompatActivity() {
             weidianbuyer://wdb/goods_detail?itemId=2675728831&wfr=hongbaowt&ffr=hongbaowt
             https://wdb-applink.weidian.com/?targetUrl=https%3A%2F%2Fweidian.com%2Fitem.html%3FitemID%3D2675728831%26wfr%3Dhongbaowt%26ffr%3Dhongbaowt
              */
-            intent.data = Uri.parse("weidianbuyer://wdb/goods_detail?itemId=2675728831&wfr=hongbaowt&ffr=hongbaowt")
+
+//            intent.data = Uri.parse("weidianbuyer://wdb/goods_detail?itemId=2675728831&wfr=hongbaowt&ffr=hongbaowt")
+            intent.data = Uri.parse("weidianbuyer://wdb/goods_detail?itemId=4420472834&wfr=hongbaowt&ffr=hongbaowt&adsk=shop;12533892;cpc;outer_bytedance;1633991388376_7WTVUExkcxobqtme_9JemRxttyX8KdcKlOU8J4atHWI2AMITrucKyAQAbo0VnkXpJhN0L3-1aCmhK1v0rVRKbY_EX-1MLRIfWnye8A4l7LbWxzFhuLmt89mQ_pvkhI284aGLtVnUUQL7jxazPMCc2YnDW9hudUqjYKlrxgCf0RTV2FHSs9LRhQiL5gQ91qTin3KL20QQ1g7HGHwLPMdVGBJ-aVoUfTo-d6ju4EFxoidgxwcszIHs3C7xbqZFyoBQlPEjjb_tpRa1DLLve_a941df7f314acb7b145989dd0ee705ede7de47e4679c22f16d45d32a5e1c0513;;1;;1612834184&adid=1713327140526123&cid=1713327569212468&csite=900000000&union_site=2033739452&requestid=ef753cc0-a427-4a61-99dd-916016b9b70fu8412&callback_url=http%3A%2F%2Fad.toutiao.com%2Ftrack%2Factivate%2F%3Fcallback%3DCKuw4fywyIUDELSolsmyyIUDGI2LoNi69EUgvrabnIcCKAAwDDjclu_CA0IpZWY3NTNjYzAtYTQyNy00YTYxLTk5ZGQtOTE2MDE2YjliNzBmdTg0MTJIgNKTrQNQAJABAA%26os%3D0%26muid%3Dd64bc3753df361f6a937b46f2b5c029d&backurl=__back_url__")
+
+
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
         }
